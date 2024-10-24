227bf3f2ca1292b62b76d0781bf2423330f26f68
Justin Jemison <jjemi8884@gmail.com>
2024-10-23 21:09:45 -0700

    Got API working!

---
+++ js/foul.js
@@ -0,0 +1,78 @@
+//Duck weather
+const btnWeather = document.getElementById("btnWeather");
+const disWeather = document.getElementById("weatherDiv");
+const zipWeahter = document.getElementById("zipWeather");
+const zipCodeBx = document.getElementById("zipcode");
+const zipSec = document.getElementById("zipSection");
+
+btnWeather.addEventListener('click', getWeatherURL);
+
+function getWeatherURL(){
+    
+    
+    let url = "https://api.weather.gov/points/47.2355,-122.1292";
+    
+    fetch(url)
+    .then((response) => {
+        if (!response.ok){
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        getLocalWeather(data.properties.forecast);
+    })
+   
+}
+
+function getLocalWeather(url){
+    fetch(url)
+    .then((response) => {
+        if(!response.ok){
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+            let forcast = data.properties.periods[0];
+            updateFoulWeather(forcast);
+        })
+}
+
+function updateFoulWeather(forcast){
+    let temp = forcast.temperature;
+    let rainPercent = forcast.probabilityOfPrecipitation.value;
+    let wind = forcast.windSpeed;
+    disWeather.innerHTML = `
+    <p>Current Temp is ${temp}</p>
+    <p>${rainPercent}% chance of rain</p>
+    <p>Current wind is ${wind}
+    <div>${duckMood(temp, rainPercent,wind)}</div>
+    `;
+    zipSec.classList.remove("hidden");
+}
+
+function duckMood(temp, rain, wind){
+    let re = new RegExp("\d")
+    let intWind = re.exec(wind);
+    if(temp > 30 && temp < 80 && rain > 50 && intWind < 10){
+       
+        return `<h2> This is a amazing duck day, good chance of rain, not to hot and no snow</h2>
+                <img src= "images/Happy Duck.png" alt="A happy mallard duck with its beak open"/>
+                <p>image source: https://img.fotocommunity.com/a-happy-duck-is-a-happy-duck-cc3352a8-32a5-4557-a9a2-3b18fa45271a.jpg?height=1080</p> 
+                `
+    }else if (temp > 30 && temp < 80 && intWind < 10  ){
+        return `<h2> This is good duck day, no rain, not to hot and no snow, a good lazy day</h2>
+                <img src= "images/meDuck.png" alt="A sleeping white puff duck"/>
+                <p>image source: Shot by me, that is the famous duck Puffy</p> 
+                `
+    }else {
+        return `<h2> This is not a good duck day and we are just waiting for it to be over!</h2>
+                <img src = "images/madDuck.png" alt="a image of a mad duck looking at the water" />
+                <p>image source: https://live.staticflickr.com/4096/4936089974_6cdd131d91_b.jpg </p>
+                
+                `
+
+    }
+}
+  
--- js/index.js
+++ js/index.js
@@ -1,18 +1,57 @@
 
 
 const imagesScroll =[
-    "images/_NIK0762.JPG",
-    "images/_NIK0814.JPG",
-    "images/_NIK0955.JPG",
-    "images/Screenshot 2024-10-15 162859.png"
-];
+    
+    "images/_NIK0252.JPG",
+    "images/_NIK0579.JPG",
+    "images/_NIK0580.JPG",
+    "images/_NIK0581.JPG",
+    "images/_NIK0582.JPG",
+    "images/_NIK0583.JPG",
+    "images/_NIK0586.JPG",
+    "images/_NIK0588.JPG",
+    "images/_NIK0589.JPG",
+    "images/_NIK0591.JPG",
+    "images/_NIK0597.JPG",
+    "images/_NIK0598.JPG",
+    "images/_NIK0599.JPG",
+    "images/_NIK0600.JPG",
+    "images/_NIK0601.JPG",
+    "images/_NIK0602.JPG",
+    "images/_NIK0603.JPG",
+    "images/_NIK0604.JPG",
+    "images/_NIK0605.JPG",
+    "images/_NIK0606.JPG",
+    "images/_NIK0608.JPG",
+    "images/_NIK0609.JPG",
+    "images/_NIK0611.JPG",
+    "images/_NIK0612.JPG",
+    "images/_NIK0614.JPG",
+    "images/_NIK0615.JPG",
+    "images/_NIK0616.JPG",
+    "images/_NIK0618.JPG",
+    "images/_NIK0635.JPG",
+    "images/_NIK0636.JPG",
+    "images/_NIK0643.JPG",
+    "images/_NIK0654.JPG",
+    "images/_NIK0649.JPG",
+    "images/_NIK0653.JPG",
+    "images/_NIK0673.JPG",
+    "images/_NIK0674.JPG",
+    "images/_NIK0675.JPG",
+    "images/_NIK0775.JPG",
+    "images/_NIK0809.JPG",
+    "images/_NIK0858.JPG",
+    "images/_NIK0864.JPG",
+    "images/_NIK0871.JPG",
+    "images/_NIK0873.JPG",
+    "images/_NIK0904.JPG"
+  ]
 
 let scrollIndex = 0;
 
 const scrollImg = document.getElementById("imgScroll");
 const scrollSection = document.getElementById("pageScroll")
-
-let scrollInterval = setInterval(slideShow, 4000);
 
 let showOn = false;
 
@@ -27,6 +66,7 @@ let btnScroll = document.getElementById("btnScroll");
 btnScroll.addEventListener('click', toggleImg);
 
 function toggleImg(){
+    console.log('show stopped');
     if(showOn) {
         
         btnScroll.innerHTML = 'Start Show';
@@ -41,4 +81,7 @@ function toggleImg(){
         
     }
 }
+
+
+    
 
